d87c16c805a33858286896b6a0535d59d6e27896
Max Zapara <max.e.zapara@gmail.com>
2023-09-21 21:29:15 +0300

    Added HTML files and virtual environment

--- FirstServer/main.py
+++ FirstServer/main.py
@@ -1,11 +1,12 @@
 from glob import escape
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World! I`m Max(OrIgInSzz)</p>"
+    return render_template('Pori roky.html')
 
 @app.route("/hello/<name>")
 def hello(name):
@@ -14,5 +15,12 @@ def hello(name):
 @app.route("/bye/<name>")
 def bye(name):
     return f"Bye, {escape(name)}!"
+
+
+
+'''@app.route("/main")
+def main():
+    return "<p>The Main(base)</p>
+    "<p>Hello, World! I`m Max(OrIgInSzz)</p>""'''
 
 app.run()
