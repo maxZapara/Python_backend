95d821305da8d1b181320e4534dca6d1d305a206
Max Zapara <max.e.zapara@gmail.com>
2023-10-17 22:34:47 +0300

    Version 0.4

---
+++ FirstServer/api.py
@@ -0,0 +1,36 @@
+import requests
+headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNzRjMzE3NWJjMGExNzNiMDkwZjkyZTljMjQ3NzRmNyIsInN1YiI6IjY0NzBlM2NmNzcwNzAwMDBkZjE0MDFjYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Y3zfcONHo2VXJV_CQbXmR56Kw0YqR296Bvqz_HbcbGU"
+}
+
+def get_upcoming():
+    global headers
+    url = "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1"
+    response = requests.get(url, headers=headers)
+
+    if response.status_code==200:
+        data=response.json()
+        return data.get('results')
+    return[]
+
+def get_images_base_path():
+    global headers
+    url = "https://api.themoviedb.org/3/configuration"
+    response = requests.get(url, headers=headers)
+
+    if response.status_code==200:
+        data=response.json()
+        return data.get('images').get('base_url')
+    return('')
+
+def get_popular(page=1):
+    global headers
+    url = f"https://api.themoviedb.org/3/movie/popular?language=en-US&page={page}"
+    response = requests.get(url, headers=headers)
+    if response.status_code==200:
+        data=response.json()
+        return data.get('results')
+    return []
+# d=get_upcoming()
+# b=get_images_base_path()
--- FirstServer/main.py
+++ FirstServer/main.py
@@ -1,21 +1,27 @@
 from glob import escape
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from api import get_upcoming,get_images_base_path,get_popular
 
 
 app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
 def hello_world():
-    return render_template('First.html')
+    movies=get_upcoming()
+    popular=get_popular()
+    return render_template('First.html', movies=movies[0:4], popular=popular[0:4])
 
-@app.route("/hello/<name>")
-def hello(name):
-    return f"Hello, {escape(name)}!"
 
-@app.route("/bye/<name>")
-def bye(name):
-    return f"Bye, {escape(name)}!"
+headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNzRjMzE3NWJjMGExNzNiMDkwZjkyZTljMjQ3NzRmNyIsInN1YiI6IjY0NzBlM2NmNzcwNzAwMDBkZjE0MDFjYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Y3zfcONHo2VXJV_CQbXmR56Kw0YqR296Bvqz_HbcbGU"
+}
 
+@app.route("/popular")
+def base_path():
+    page=request.args.get('page',1)
+    movies=get_popular(page)
+    return render_template('movie_list.html', movies=movies)
 
 
 '''@app.route("/main")
