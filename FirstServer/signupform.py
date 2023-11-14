60ee8d277dfa51c8002d07dae81af50c157be6a4
Max Zapara <max.e.zapara@gmail.com>
2023-11-14 21:22:20 +0200

    0.8 Version

--- FirstServer/signupform.py
+++ FirstServer/signupform.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, EmailField
+from wtforms.validators import DataRequired, Email, Length
 class MyForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired(),Length(min=5, max=20)])
+    email = EmailField('Email', validators=[DataRequired(),Email()])
+    password = PasswordField('Password', validators=[DataRequired(),Length(min=4, max=13)])
